fc31f4ea436020ae1af0fecd44abfdb09ae50dc7
matthewjmckinley <matthewjamesmckinley@gmail.com>
2026-06-08 14:40:14 -0700

    Run CS Fixer

--- src/Site/BlockLayout/Carousel.php
+++ src/Site/BlockLayout/Carousel.php
@@ -17,7 +17,7 @@ class Carousel extends AbstractBlockLayout
     }
 
     public function form(PhpRenderer $view, SiteRepresentation $site,
-        SitePageRepresentation $page = null, SitePageBlockRepresentation $block = null
+        ?SitePageRepresentation $page = null, ?SitePageBlockRepresentation $block = null
     ) {
         $defaults = [
             'carouselHeading' => '',
